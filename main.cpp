1aca4dcc1a09d20ebe35e1fc8a089f46632f51d0
Markus Klemm <markus.klemm@watttron.com>
2020-10-22 11:51:38 +0200

    Look if it compiles on MSVC

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <optional>
+
 #include <sqlite_modern_cpp.h>
 
 sqlite::database& get_database() {
@@ -8,11 +10,26 @@ sqlite::database& get_database() {
 }
 
 void create_test_database() {
+    auto db = get_database();
+
+    db << "CREATE TABLE IF NOT EXISTS test (id integer not null primary key, opt_field real null) without rowid;";
 
 }
 
-int main () {
+void insert_data() {
+    long id {1337};
+    auto opt_field = std::optional<double>(4.2);
+    auto db = get_database();
 
+    db << "insert or replace into test(id,opt_field) values (?,?);"
+    << id << opt_field;
+}
+
+
+
+int main () {
+    create_test_database();
+    insert_data();
 
 }
 
